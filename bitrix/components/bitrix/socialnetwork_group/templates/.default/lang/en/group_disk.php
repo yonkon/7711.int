29d8658c82ccc2a4079eb2fe27019753d00d57e6
X-iLeR <deadlove1993@gmail.com>
2015-11-30 23:05:50 +0200

    a

---
+++ bitrix/components/bitrix/socialnetwork_group/templates/.default/lang/en/group_disk.php
@@ -0,0 +1,5 @@
+<?
+$MESS["DISK_UPLOAD_WITH_FILE_DRAG_AND_DROP"] = "Drag attachments here";
+$MESS["DISK_UPLOAD_FILE_OR_IMAGE"] = "Upload files<br />and images";
+$MESS["DISK_VIEW_SMALL_DETAIL_SIDEBAR"] = "Select a file or folder <br> to view properties.";
+?>
