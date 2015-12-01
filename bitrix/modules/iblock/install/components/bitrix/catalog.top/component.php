593fc70a960623fadafde128360dd0c70face47d
X-iLeR <deadlove1993@gmail.com>
2015-12-01 03:03:50 +0200

    a

--- bitrix/modules/iblock/install/components/bitrix/catalog.top/component.php
+++ bitrix/modules/iblock/install/components/bitrix/catalog.top/component.php
@@ -393,4 +393,480 @@ if($this->StartResultCache(false, array($arrFilter, ($arParams["CACHE_GROUPS"]==
 		"SORT",
 		"PREVIEW_TEXT",
 		"PREVIEW_TEXT_TYPE",
-		"DETAIL_TEX
+		"DETAIL_TEXT",
+		"DETAIL_TEXT_TYPE",
+		"DATE_CREATE",
+		"CREATED_BY",
+		"TIMESTAMP_X",
+		"MODIFIED_BY",
+		"TAGS",
+		"IBLOCK_SECTION_ID",
+		"DETAIL_PAGE_URL",
+		"DETAIL_PICTURE",
+		"PREVIEW_PICTURE",
+	);
+	//WHERE
+	$arrFilter["ACTIVE"] = "Y";
+	if($arParams["IBLOCK_ID"] > 0)
+		$arrFilter["IBLOCK_ID"] = $arParams["IBLOCK_ID"];
+	$arrFilter["IBLOCK_LID"] = SITE_ID;
+	$arrFilter["IBLOCK_ACTIVE"] = "Y";
+	$arrFilter["ACTIVE_DATE"] = "Y";
+	$arrFilter["ACTIVE"] = "Y";
+	$arrFilter["CHECK_PERMISSIONS"] = "Y";
+	if ($bIBlockCatalog && 'Y' == $arParams['HIDE_NOT_AVAILABLE'])
+		$arrFilter['CATALOG_AVAILABLE'] = 'Y';
+
+	//ORDER BY
+	$arSort = array(
+		$arParams["ELEMENT_SORT_FIELD"] => $arParams["ELEMENT_SORT_ORDER"],
+		$arParams["ELEMENT_SORT_FIELD2"] => $arParams["ELEMENT_SORT_ORDER2"],
+	);
+	//PRICES
+	$arPriceTypeID = array();
+	if (!$arParams["USE_PRICE_COUNT"])
+	{
+		foreach($arResult["PRICES"] as &$value)
+		{
+			if (!$value['CAN_VIEW'] && !$value['CAN_BUY'])
+				continue;
+			$arSelect[] = $value["SELECT"];
+			$arrFilter["CATALOG_SHOP_QUANTITY_".$value["ID"]] = $arParams["SHOW_PRICE_COUNT"];
+		}
+		if (isset($value))
+			unset($value);
+	}
+	else
+	{
+		foreach($arResult["PRICES"] as &$value)
+		{
+			if (!$value['CAN_VIEW'] && !$value['CAN_BUY'])
+				continue;
+			$arPriceTypeID[] = $value["ID"];
+		}
+		if (isset($value))
+			unset($value);
+	}
+
+	$arDefaultMeasure = array();
+	if ($bIBlockCatalog)
+		$arDefaultMeasure = CCatalogMeasure::getDefaultMeasure(true, true);
+	$arCurrencyList = array();
+
+	$bGetPropertyCodes = !empty($arParams["PROPERTY_CODE"]);
+	$bGetProductProperties = !empty($arParams["PRODUCT_PROPERTIES"]);
+	$bGetProperties = $bGetPropertyCodes || $bGetProductProperties;
+
+	$arResult["ITEMS"] = array();
+	$arMeasureMap = array();
+	$intKey = 0;
+	$arElementLink = array();
+	$rsElements = CIBlockElement::GetList($arSort, $arrFilter, false, array("nTopCount" => $arParams["ELEMENT_COUNT"]), $arSelect);
+	$rsElements->SetUrlTemplates($arParams["DETAIL_URL"]);
+
+	while($arItem = $rsElements->GetNext())
+	{
+		$arItem['ID'] = intval($arItem['ID']);
+
+		$arItem['ACTIVE_FROM'] = $arItem['DATE_ACTIVE_FROM'];
+		$arItem['ACTIVE_TO'] = $arItem['DATE_ACTIVE_TO'];
+
+		$arButtons = CIBlock::GetPanelButtons(
+			$arItem["IBLOCK_ID"],
+			$arItem["ID"],
+			$arItem["IBLOCK_SECTION_ID"],
+			array("SECTION_BUTTONS"=>false, "SESSID"=>false, "CATALOG"=>true)
+		);
+		$arItem["EDIT_LINK"] = $arButtons["edit"]["edit_element"]["ACTION_URL"];
+		$arItem["DELETE_LINK"] = $arButtons["edit"]["delete_element"]["ACTION_URL"];
+
+		$ipropValues = new \Bitrix\Iblock\InheritedProperty\ElementValues($arItem["IBLOCK_ID"], $arItem["ID"]);
+		$arItem["IPROPERTY_VALUES"] = $ipropValues->getValues();
+
+		$arItem["PREVIEW_PICTURE"] = (0 < $arItem["PREVIEW_PICTURE"] ? CFile::GetFileArray($arItem["PREVIEW_PICTURE"]) : false);
+		if ($arItem["PREVIEW_PICTURE"])
+		{
+			$arItem["PREVIEW_PICTURE"]["ALT"] = $arItem["IPROPERTY_VALUES"]["ELEMENT_PREVIEW_PICTURE_FILE_ALT"];
+			if ($arItem["PREVIEW_PICTURE"]["ALT"] == "")
+				$arItem["PREVIEW_PICTURE"]["ALT"] = $arItem["NAME"];
+			$arItem["PREVIEW_PICTURE"]["TITLE"] = $arItem["IPROPERTY_VALUES"]["ELEMENT_PREVIEW_PICTURE_FILE_TITLE"];
+			if ($arItem["PREVIEW_PICTURE"]["TITLE"] == "")
+				$arItem["PREVIEW_PICTURE"]["TITLE"] = $arItem["NAME"];
+		}
+		$arItem["DETAIL_PICTURE"] = (0 < $arItem["DETAIL_PICTURE"] ? CFile::GetFileArray($arItem["DETAIL_PICTURE"]) : false);
+		if ($arItem["DETAIL_PICTURE"])
+		{
+			$arItem["DETAIL_PICTURE"]["ALT"] = $arItem["IPROPERTY_VALUES"]["ELEMENT_DETAIL_PICTURE_FILE_ALT"];
+			if ($arItem["DETAIL_PICTURE"]["ALT"] == "")
+				$arItem["DETAIL_PICTURE"]["ALT"] = $arItem["NAME"];
+			$arItem["DETAIL_PICTURE"]["TITLE"] = $arItem["IPROPERTY_VALUES"]["ELEMENT_DETAIL_PICTURE_FILE_TITLE"];
+			if ($arItem["DETAIL_PICTURE"]["TITLE"] == "")
+				$arItem["DETAIL_PICTURE"]["TITLE"] = $arItem["NAME"];
+		}
+
+		$arItem["PROPERTIES"] = array();
+		$arItem["DISPLAY_PROPERTIES"] = array();
+		$arItem["PRODUCT_PROPERTIES"] = array();
+		$arItem['PRODUCT_PROPERTIES_FILL'] = array();
+
+		if ($bIBlockCatalog)
+		{
+			if (!isset($arItem["CATALOG_MEASURE_RATIO"]))
+				$arItem["CATALOG_MEASURE_RATIO"] = 1;
+			if (!isset($arItem['CATALOG_MEASURE']))
+				$arItem['CATALOG_MEASURE'] = 0;
+			$arItem['CATALOG_MEASURE'] = intval($arItem['CATALOG_MEASURE']);
+			if (0 > $arItem['CATALOG_MEASURE'])
+				$arItem['CATALOG_MEASURE'] = 0;
+			if (!isset($arItem['CATALOG_MEASURE_NAME']))
+				$arItem['CATALOG_MEASURE_NAME'] = '';
+
+			$arItem['CATALOG_MEASURE_NAME'] = $arDefaultMeasure['SYMBOL_RUS'];
+			$arItem['~CATALOG_MEASURE_NAME'] = $arDefaultMeasure['~SYMBOL_RUS'];
+			if (0 < $arItem['CATALOG_MEASURE'])
+			{
+				if (!isset($arMeasureMap[$arItem['CATALOG_MEASURE']]))
+					$arMeasureMap[$arItem['CATALOG_MEASURE']] = array();
+				$arMeasureMap[$arItem['CATALOG_MEASURE']][] = $intKey;
+			}
+		}
+		$arResult["ITEMS"][$intKey] = $arItem;
+		$arResult["ELEMENTS"][$intKey] = $arItem["ID"];
+		$arElementLink[$arItem['ID']] = &$arResult["ITEMS"][$intKey];
+		$intKey++;
+	}
+	$arResult['MODULES'] = $arResultModules;
+
+	if (!empty($arResult["ELEMENTS"]) && ($bGetProperties || ($bCatalog && $boolNeedCatalogCache)))
+	{
+		$arPropFilter = array(
+			'ID' => $arResult["ELEMENTS"],
+			'IBLOCK_ID' => $arParams['IBLOCK_ID']
+		);
+		CIBlockElement::GetPropertyValuesArray($arElementLink, $arParams["IBLOCK_ID"], $arPropFilter);
+
+		foreach ($arResult["ITEMS"] as &$arItem)
+		{
+			if ($bCatalog && $boolNeedCatalogCache)
+			{
+				CCatalogDiscount::SetProductPropertiesCache($arItem['ID'], $arItem["PROPERTIES"]);
+			}
+
+			if ($bGetProperties)
+			{
+				foreach($arParams["PROPERTY_CODE"] as $pid)
+				{
+					if (!isset($arItem["PROPERTIES"][$pid]))
+						continue;
+					$prop = &$arItem["PROPERTIES"][$pid];
+					$boolArr = is_array($prop["VALUE"]);
+					if(
+						($boolArr && !empty($prop["VALUE"]))
+						|| (!$boolArr && strlen($prop["VALUE"]) > 0)
+					)
+					{
+						$arItem["DISPLAY_PROPERTIES"][$pid] = CIBlockFormatProperties::GetDisplayValue($arItem, $prop, "catalog_out");
+					}
+				}
+
+				if ($bGetProductProperties)
+				{
+					$arItem["PRODUCT_PROPERTIES"] = CIBlockPriceTools::GetProductProperties(
+						$arParams["IBLOCK_ID"],
+						$arItem["ID"],
+						$arParams["PRODUCT_PROPERTIES"],
+						$arItem["PROPERTIES"]
+					);
+					if (!empty($arItem["PRODUCT_PROPERTIES"]))
+					{
+						$arItem['PRODUCT_PROPERTIES_FILL'] = CIBlockPriceTools::getFillProductProperties($arItem['PRODUCT_PROPERTIES']);
+					}
+				}
+			}
+		}
+		if (isset($arItem))
+			unset($arItem);
+	}
+
+	if ($bIBlockCatalog)
+	{
+		if (!empty($arResult["ELEMENTS"]))
+		{
+			$rsRatios = CCatalogMeasureRatio::getList(
+				array(),
+				array('PRODUCT_ID' => $arResult["ELEMENTS"]),
+				false,
+				false,
+				array('PRODUCT_ID', 'RATIO')
+			);
+			while ($arRatio = $rsRatios->Fetch())
+			{
+				$arRatio['PRODUCT_ID'] = intval($arRatio['PRODUCT_ID']);
+				if (isset($arElementLink[$arRatio['PRODUCT_ID']]))
+				{
+					$intRatio = intval($arRatio['RATIO']);
+					$dblRatio = doubleval($arRatio['RATIO']);
+					$mxRatio = ($dblRatio > $intRatio ? $dblRatio : $intRatio);
+					if (CATALOG_VALUE_EPSILON > abs($mxRatio))
+						$mxRatio = 1;
+					elseif (0 > $mxRatio)
+						$mxRatio = 1;
+					$arElementLink[$arRatio['PRODUCT_ID']]['CATALOG_MEASURE_RATIO'] = $mxRatio;
+				}
+			}
+		}
+		if (!empty($arMeasureMap))
+		{
+			$rsMeasures = CCatalogMeasure::getList(
+				array(),
+				array('@ID' => array_keys($arMeasureMap)),
+				false,
+				false,
+				array('ID', 'SYMBOL_RUS')
+			);
+			while ($arMeasure = $rsMeasures->GetNext())
+			{
+				$arMeasure['ID'] = intval($arMeasure['ID']);
+				if (isset($arMeasureMap[$arMeasure['ID']]) && !empty($arMeasureMap[$arMeasure['ID']]))
+				{
+					foreach ($arMeasureMap[$arMeasure['ID']] as &$intOneKey)
+					{
+						$arResult['ITEMS'][$intOneKey]['CATALOG_MEASURE_NAME'] = $arMeasure['SYMBOL_RUS'];
+						$arResult['ITEMS'][$intOneKey]['~CATALOG_MEASURE_NAME'] = $arMeasure['~SYMBOL_RUS'];
+					}
+					unset($intOneKey);
+				}
+			}
+		}
+	}
+	if ($bCatalog && $boolNeedCatalogCache && !empty($arResult["ELEMENTS"]))
+	{
+		CCatalogDiscount::SetProductSectionsCache($arResult["ELEMENTS"]);
+		CCatalogDiscount::SetDiscountProductCache($arResult["ELEMENTS"], array('IBLOCK_ID' => $arParams["IBLOCK_ID"], 'GET_BY_ID' => 'Y'));
+	}
+	if (isset($arItem))
+		unset($arItem);
+
+	$currentPath = CHTTP::urlDeleteParams(
+		$APPLICATION->GetCurPageParam(),
+		array($arParams['PRODUCT_ID_VARIABLE'], $arParams['ACTION_VARIABLE']),
+		array("delete_system_params" => true)
+	);
+	$currentPath .= (stripos($currentPath, '?') === false ? '?' : '&');
+	if ($arParams['COMPARE_PATH'] == '')
+	{
+		$comparePath = $currentPath;
+	}
+	else
+	{
+		$comparePath = $arParams['COMPARE_PATH'];
+		$comparePath .= (stripos($comparePath, '?') === false ? '?' : '&');
+	}
+
+	$arResult['~BUY_URL_TEMPLATE'] = $currentPath.$arParams["ACTION_VARIABLE"]."=BUY&".$arParams["PRODUCT_ID_VARIABLE"]."=#ID#";
+	$arResult['BUY_URL_TEMPLATE'] = htmlspecialcharsbx($arResult['~BUY_URL_TEMPLATE']);
+	$arResult['~ADD_URL_TEMPLATE'] = $currentPath.$arParams["ACTION_VARIABLE"]."=ADD2BASKET&".$arParams["PRODUCT_ID_VARIABLE"]."=#ID#";
+	$arResult['ADD_URL_TEMPLATE'] = htmlspecialcharsbx($arResult['~ADD_URL_TEMPLATE']);
+	$arResult['~SUBSCRIBE_URL_TEMPLATE'] = $currentPath.$arParams["ACTION_VARIABLE"]."=SUBSCRIBE_PRODUCT&".$arParams["PRODUCT_ID_VARIABLE"]."=#ID#";
+	$arResult['SUBSCRIBE_URL_TEMPLATE'] = htmlspecialcharsbx($arResult['~SUBSCRIBE_URL_TEMPLATE']);
+	$arResult['~COMPARE_URL_TEMPLATE'] = $comparePath.$arParams["ACTION_VARIABLE"]."=ADD_TO_COMPARE_LIST&".$arParams["PRODUCT_ID_VARIABLE"]."=#ID#";
+	$arResult['COMPARE_URL_TEMPLATE'] = htmlspecialcharsbx($arResult['~COMPARE_URL_TEMPLATE']);
+	unset($comparePath, $currentPath);
+
+	foreach ($arResult["ITEMS"] as &$arItem)
+	{
+		$arItem["PRICES"] = array();
+		$arItem["PRICE_MATRIX"] = false;
+		$arItem['MIN_PRICE'] = false;
+		if($arParams["USE_PRICE_COUNT"])
+		{
+			if ($bCatalog)
+			{
+				$arItem["PRICE_MATRIX"] = CatalogGetPriceTableEx($arItem["ID"], 0, $arPriceTypeID, 'Y', $arConvertParams);
+				if (isset($arItem["PRICE_MATRIX"]["COLS"]) && is_array($arItem["PRICE_MATRIX"]["COLS"]))
+				{
+					foreach($arItem["PRICE_MATRIX"]["COLS"] as $keyColumn=>$arColumn)
+						$arItem["PRICE_MATRIX"]["COLS"][$keyColumn]["NAME_LANG"] = htmlspecialcharsbx($arColumn["NAME_LANG"]);
+				}
+			}
+		}
+		else
+		{
+			$arItem["PRICES"] = CIBlockPriceTools::GetItemPrices($arParams["IBLOCK_ID"], $arResult["PRICES"], $arItem, $arParams['PRICE_VAT_INCLUDE'], $arConvertParams);
+			if (!empty($arItem["PRICES"]))
+			{
+				foreach ($arItem['PRICES'] as &$arOnePrice)
+				{
+					if ('Y' == $arOnePrice['MIN_PRICE'])
+					{
+						$arItem['MIN_PRICE'] = $arOnePrice;
+						break;
+					}
+				}
+				unset($arOnePrice);
+			}
+		}
+		$arItem["CAN_BUY"] = CIBlockPriceTools::CanBuy($arParams["IBLOCK_ID"], $arResult["PRICES"], $arItem);
+
+		$arItem['~BUY_URL'] = str_replace('#ID#', $arItem["ID"], $arResult['~BUY_URL_TEMPLATE']);
+		$arItem['BUY_URL'] = str_replace('#ID#', $arItem["ID"], $arResult['BUY_URL_TEMPLATE']);
+		$arItem['~ADD_URL'] = str_replace('#ID#', $arItem["ID"], $arResult['~ADD_URL_TEMPLATE']);
+		$arItem['ADD_URL'] = str_replace('#ID#', $arItem["ID"], $arResult['ADD_URL_TEMPLATE']);
+		$arItem['~SUBSCRIBE_URL'] = str_replace('#ID#', $arItem["ID"], $arResult['~SUBSCRIBE_URL_TEMPLATE']);
+		$arItem['SUBSCRIBE_URL'] = str_replace('#ID#', $arItem["ID"], $arResult['SUBSCRIBE_URL_TEMPLATE']);
+		if ($arParams['DISPLAY_COMPARE'])
+		{
+			$arItem['~COMPARE_URL'] = str_replace('#ID#', $arItem["ID"], $arResult['~COMPARE_URL_TEMPLATE']);
+			$arItem['COMPARE_URL'] = str_replace('#ID#', $arItem["ID"], $arResult['COMPARE_URL_TEMPLATE']);
+		}
+
+		if ('Y' == $arParams['CONVERT_CURRENCY'])
+		{
+			if ($arParams["USE_PRICE_COUNT"])
+			{
+				if (is_array($arItem["PRICE_MATRIX"]) && !empty($arItem["PRICE_MATRIX"]))
+				{
+					if (isset($arItem["PRICE_MATRIX"]['CURRENCY_LIST']) && is_array($arItem["PRICE_MATRIX"]['CURRENCY_LIST']))
+						$arCurrencyList = array_merge($arCurrencyList, $arItem["PRICE_MATRIX"]['CURRENCY_LIST']);
+				}
+			}
+			else
+			{
+				if (!empty($arItem["PRICES"]))
+				{
+					foreach ($arItem["PRICES"] as &$arOnePrices)
+					{
+						if (isset($arOnePrices['ORIG_CURRENCY']))
+							$arCurrencyList[] = $arOnePrices['ORIG_CURRENCY'];
+					}
+					if (isset($arOnePrices))
+						unset($arOnePrices);
+				}
+			}
+		}
+	}
+	if (isset($arItem))
+		unset($arItem);
+
+	if(!isset($arParams["OFFERS_FIELD_CODE"]))
+		$arParams["OFFERS_FIELD_CODE"] = array();
+	elseif (!is_array($arParams["OFFERS_FIELD_CODE"]))
+		$arParams["OFFERS_FIELD_CODE"] = array($arParams["OFFERS_FIELD_CODE"]);
+	foreach($arParams["OFFERS_FIELD_CODE"] as $key => $value)
+		if($value === "")
+			unset($arParams["OFFERS_FIELD_CODE"][$key]);
+
+	if(!isset($arParams["OFFERS_PROPERTY_CODE"]))
+		$arParams["OFFERS_PROPERTY_CODE"] = array();
+	elseif (!is_array($arParams["OFFERS_PROPERTY_CODE"]))
+		$arParams["OFFERS_PROPERTY_CODE"] = array($arParams["OFFERS_PROPERTY_CODE"]);
+	foreach($arParams["OFFERS_PROPERTY_CODE"] as $key => $value)
+		if($value === "")
+			unset($arParams["OFFERS_PROPERTY_CODE"][$key]);
+
+	if(
+		$bCatalog
+		&& !empty($arResult["ELEMENTS"])
+		&& (
+			!empty($arParams["OFFERS_FIELD_CODE"])
+			|| !empty($arParams["OFFERS_PROPERTY_CODE"])
+		)
+	)
+	{
+		$offersFilter = array(
+			'IBLOCK_ID' => $arParams['IBLOCK_ID'],
+			'HIDE_NOT_AVAILABLE' => $arParams['HIDE_NOT_AVAILABLE']
+		);
+		if (!$arParams["USE_PRICE_COUNT"])
+		{
+			$offersFilter['SHOW_PRICE_COUNT'] = $arParams['SHOW_PRICE_COUNT'];
+		}
+		$arOffers = CIBlockPriceTools::GetOffersArray(
+			$offersFilter,
+			$arResult["ELEMENTS"],
+			array(
+				$arParams["OFFERS_SORT_FIELD"] => $arParams["OFFERS_SORT_ORDER"],
+				$arParams["OFFERS_SORT_FIELD2"] => $arParams["OFFERS_SORT_ORDER2"],
+			),
+			$arParams["OFFERS_FIELD_CODE"],
+			$arParams["OFFERS_PROPERTY_CODE"],
+			$arParams["OFFERS_LIMIT"],
+			$arResult["PRICES"],
+			$arParams['PRICE_VAT_INCLUDE'],
+			$arConvertParams
+		);
+		if(!empty($arOffers))
+		{
+			foreach ($arResult["ELEMENTS"] as $id)
+			{
+				$arElementLink[$id]['OFFERS'] = array();
+			}
+
+			foreach($arOffers as $arOffer)
+			{
+				if (isset($arElementLink[$arOffer["LINK_ELEMENT_ID"]]))
+				{
+					$arOffer['~BUY_URL'] = str_replace('#ID#', $arOffer["ID"], $arResult['~BUY_URL_TEMPLATE']);
+					$arOffer['BUY_URL'] = str_replace('#ID#', $arOffer["ID"], $arResult['BUY_URL_TEMPLATE']);
+					$arOffer['~ADD_URL'] = str_replace('#ID#', $arOffer["ID"], $arResult['~ADD_URL_TEMPLATE']);
+					$arOffer['ADD_URL'] = str_replace('#ID#', $arOffer["ID"], $arResult['ADD_URL_TEMPLATE']);
+					if ($arParams['DISPLAY_COMPARE'])
+					{
+						$arOffer['~COMPARE_URL'] = str_replace('#ID#', $arOffer["ID"], $arResult['~COMPARE_URL_TEMPLATE']);
+						$arOffer['COMPARE_URL'] = str_replace('#ID#', $arOffer["ID"], $arResult['COMPARE_URL_TEMPLATE']);
+					}
+					$arOffer['~SUBSCRIBE_URL'] = str_replace('#ID#', $arOffer["ID"], $arResult['~SUBSCRIBE_URL_TEMPLATE']);
+					$arOffer['SUBSCRIBE_URL'] = str_replace('#ID#', $arOffer["ID"], $arResult['SUBSCRIBE_URL_TEMPLATE']);
+
+					$arElementLink[$arOffer["LINK_ELEMENT_ID"]]['OFFERS'][] = $arOffer;
+
+					if ('Y' == $arParams['CONVERT_CURRENCY'])
+					{
+						if (!empty($arOffer['PRICES']))
+						{
+							foreach ($arOffer['PRICES'] as &$arOnePrices)
+							{
+								if (isset($arOnePrices['ORIG_CURRENCY']))
+									$arCurrencyList[] = $arOnePrices['ORIG_CURRENCY'];
+							}
+							if (isset($arOnePrices))
+								unset($arOnePrices);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	if (
+		'Y' == $arParams['CONVERT_CURRENCY']
+		&& !empty($arCurrencyList)
+		&& defined("BX_COMP_MANAGED_CACHE")
+	)
+	{
+		$arCurrencyList[] = $arConvertParams['CURRENCY_ID'];
+		$arCurrencyList = array_unique($arCurrencyList);
+		$CACHE_MANAGER->StartTagCache($this->GetCachePath());
+		foreach ($arCurrencyList as &$strOneCurrency)
+		{
+			$CACHE_MANAGER->RegisterTag("currency_id_".$strOneCurrency);
+		}
+		if (isset($strOneCurrency))
+			unset($strOneCurrency);
+		$CACHE_MANAGER->EndTagCache();
+	}
+
+	$this->SetResultCacheKeys(array(
+	));
+	$this->IncludeComponentTemplate();
+
+	if ($bCatalog && $boolNeedCatalogCache)
+	{
+		CCatalogDiscount::ClearDiscountCache(array(
+			'PRODUCT' => true,
+			'SECTIONS' => true,
+			'PROPERTIES' => true
+		));
+	}
+}
+?>
